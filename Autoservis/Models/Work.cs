588c7fb3ceaa870c0841f6a0feea3fcf1d8115c2
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-17 12:11:28 +0200

    Added work model

---
+++ Autoservis/Models/Work.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoservis.Models
+{
+    public class Work
+    {
+        public int Id { get; set; }
+        public int Hours { get; set; }
+        public int Price { get; set; }
+        public int TotalPrice => Hours * Price;
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
