c0b03a3434a0fc2d33d5e3f00a28de198ba8819d
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-17 12:11:19 +0200

    Added photo model

---
+++ Autoservis/Models/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoservis.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FilePath { get; set; } = string.Empty;
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
