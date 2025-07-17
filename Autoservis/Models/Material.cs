a05e825bf9e49efd6ca53aa564d07554799f20f8
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-17 12:11:03 +0200

    Added material model

---
+++ Autoservis/Models/Material.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoservis.Models
+{
+    public class Material
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public MeasureUnit Unit {  get; set; }
+        public int TotalPrice => Quantity * Price;
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+
+    public enum MeasureUnit
+    {
+        ks,
+        h,
+        l
+    }
+}
