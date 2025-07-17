ba23af936fb2258edec4a139fafdf1711dfe1462
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-17 12:10:42 +0200

    Added car model

---
+++ Autoservis/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Autoservis.Models;
+
+namespace Autoservis.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Work> Works { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=Data/autoservis.db");
+        }
+    }
+}
---
+++ Autoservis/Models/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoservis.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        [Required]
+        public string BrandModel { get; set; } = string.Empty;
+        [Required]
+        public string SPZ { get; set; } = string.Empty;
+        [Required]
+        public string VIN { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string? Fuel { get; set; } = string.Empty;
+        public CarType Type {  get; set; } 
+        public string? Notes { get; set; } = string.Empty;
+
+        [Required]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+
+    public enum CarType
+    {
+        Osobní,
+        Nákladní,
+        Užitková,
+        Motocykl
+    }
+}
