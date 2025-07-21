518f4a3b584c17f7d42b06d643c86ab3eac33a4f
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-21 16:20:58 +0200

    Customer crud operations

---
+++ Autoservis/Repositories/CustomerRepostiory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autoservis.Models;
+using Autoservis.Data;
+
+namespace Autoservis.Repositories
+{
+    internal class CustomerRepostiory : ICustomerRepository
+    {
+        public readonly AppDbContext _context;
+
+        public CustomerRepostiory(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Customer customer)
+        {
+            _context.Add(customer);
+            _context.SaveChanges();
+        }
+
+        public Customer? GetById(int id)
+        {
+            return _context.Customers.Find(id);
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public void Update(Customer customer)
+        {
+            _context.Update(customer);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Autoservis/Repositories/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autoservis.Models;
+
+namespace Autoservis.Repositories
+{
+    internal class ICustomerRepository
+    {
+        void Add(Customer customer);
+        Customer? GetById(int id);
+        List<Customer> GetAll();
+        void Update(Customer customer);
+        void Delete(int id);
+    }
+}
