a9fab0363cb3c0a89fffc1d7c05d3c7677e8930c
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-25 18:38:58 +0200

    Order crud operations

---
+++ Autoservis/Repositories/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autoservis.Models;
+
+namespace Autoservis.Repositories
+{
+    public interface IOrderRepository
+    {
+        void Add(Order order);
+        Order? GetById(int id);
+        List<Order> GetAll();
+        void Update(Order order);
+        void Delete(int id);
+    }
+}
---
+++ Autoservis/Repositories/OrderRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autoservis.Models;
+using Autoservis.Data;
+
+namespace Autoservis.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _context;
+
+        public OrderRepository (AppDbContext context)
+        {
+            _context = context;
+        }
+            
+        public void Add(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+
+        public Order? GetById(int id)
+        {
+            return _context.Orders.Find(id);
+        }
+
+        public List<Order> GetAll()
+        {
+            return _context.Orders.ToList();
+        }
+
+        public void Update(Order order)
+        {
+            _context.Orders.Update(order);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var order = _context.Orders.Find(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
