1b605d26d3d0cec56da5f173dcfb2ce71ea58a3b
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-25 17:52:59 +0200

    Material crud operations

---
+++ Autoservis/Repositories/IMaterialRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autoservis.Models;
+
+namespace Autoservis.Repositories
+{
+    public interface IMaterialRepository
+    {
+        void Add(Material material);
+        Material? GetById(int id);
+        List<Material> GetAll();
+        void Update (Material material);
+        void Delete(int id);
+    }
+}
---
+++ Autoservis/Repositories/MaterialRepository.cs
@@ -0,0 +1,51 @@
+﻿using Autoservis.Data;
+using Autoservis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoservis.Repositories
+{
+    public class MaterialRepository : IMaterialRepository
+    {
+        private readonly AppDbContext _context;
+        public MaterialRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Material material)
+        {
+            _context.Materials.Add(material);
+            _context.SaveChanges();
+        }
+
+        public Material? GetById(int id)
+        {
+            return _context.Materials.Find(id);
+        }
+
+        public List<Material> GetAll()
+        {
+            return _context.Materials.ToList();
+        }
+
+        public void Update(Material material)
+        {
+            _context.Materials.Update(material);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var material = _context.Materials.Find(id);
+            if (material != null)
+            {
+                _context.Materials.Remove(material);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
