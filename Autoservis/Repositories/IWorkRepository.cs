c032eea662e5a7f6ffb01ef9da8c5f83943173bf
PetrMFIT <xmolikp00@vutbr.cz>
2025-07-30 19:24:34 +0200

    Work crud operations

---
+++ Autoservis/Repositories/IWorkRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autoservis.Models;
+
+namespace Autoservis.Repositories
+{
+    public interface IWorkRepository
+    {
+        void Add(Work work);
+        Work? GetById(int id);
+        List<Work> GetAll();
+        void Update(Work work);
+        void Delete(int id);
+    }
+}
---
+++ Autoservis/Repositories/WorkRepository.cs
@@ -0,0 +1,53 @@
+﻿using Autoservis.Data;
+using Autoservis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoservis.Repositories
+{
+    public class WorkRepository : IWorkRepository
+    {
+        public readonly AppDbContext _context;
+
+        public WorkRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Work work)
+        {
+            _context.Works.Add(work);
+            _context.SaveChanges();
+        }
+
+        public Work? GetById(int id)
+        {
+            return _context.Works.Find(id);
+        }
+
+        public List<Work> GetAll()
+        {
+            return _context.Works.ToList();
+        }
+
+        public void Update(Work work)
+        {
+            _context.Works.Update(work);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var work = _context.Works.Find(id);
+
+            if (work != null)
+            {
+                _context.Works.Remove(work);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
