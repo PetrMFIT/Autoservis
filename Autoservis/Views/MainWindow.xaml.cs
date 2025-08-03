4754569166bcb2f3e4d3db45b6e3dd224b9664d1
PetrMFIT <xmolikp00@vutbr.cz>
2025-08-03 17:02:17 +0200

    Navigation buttons

--- Autoservis/Views/MainWindow.xaml.cs
+++ Autoservis/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,25 +20,54 @@ namespace Autoservis
     public partial class MainWindow : Window
     {
         private readonly CustomerRepository customer_repo;
+        private readonly CarRepository car_repo;
         public MainWindow()
         {
             InitializeComponent();
 
             customer_repo = new CustomerRepository(App.DbContext);
+            car_repo = new CarRepository(App.DbContext);
 
-            //LoadCustomers();
+            LoadCustomers();
         }
 
-        /*private void LoadCustomers()
+        private void CustomersButton_Click(object sender, RoutedEventArgs e)
         {
-            var customers = customer_repo.GetAll();
-
-            CustomerGrid.ItemsSource = customers;
-        }*/
-
-        private void Button_Click()
-        {
-
+            LoadCustomers();
         }
+
+        private void LoadCustomers()
+        {
+            SetupCustomerColumns();
+            DataGrid.ItemsSource = customer_repo.GetAll(); ;
+        }
+
+        private void CarsButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadCars();
+        }
+
+        private void LoadCars()
+        {
+            SetupCarColumns();
+            DataGrid.ItemsSource = car_repo.GetAll();
+        }
+
+        private void SetupCustomerColumns()
+        {
+            DataGrid.Columns.Clear();
+            DataGrid.Columns.Add(new DataGridTextColumn { Header = "Jméno", Binding = new Binding("Name") });
+            DataGrid.Columns.Add(new DataGridTextColumn { Header = "Telefon", Binding = new Binding("Phone") });
+            DataGrid.Columns.Add(new DataGridTextColumn { Header = "E-mail", Binding = new Binding("Email") });
+        }
+
+        private void SetupCarColumns()
+        {
+            DataGrid.Columns.Clear();
+            DataGrid.Columns.Add(new DataGridTextColumn { Header = "Model", Binding = new Binding("BrandModel") });
+            DataGrid.Columns.Add(new DataGridTextColumn { Header = "SPZ", Binding = new Binding("SPZ") });
+            DataGrid.Columns.Add(new DataGridTextColumn { Header = "Rok", Binding = new Binding("Year") });
+        }
+
     }
 }
